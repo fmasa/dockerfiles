831ecc0b92db3359c236164a9bb672932cfed974
František Maša <frantisekmasa1@gmail.com>
2020-03-17 20:00:26 +0100

    Added PHP 7.4 image

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+docker build -t fmasa/lebeda:7.4 lebeda/7.4 --no-cache
+docker build -t fmasa/lebeda:7.4-ci lebeda/7.4-ci --no-cache
+
 docker build -t fmasa/lebeda:7.3 lebeda/7.3 --no-cache
 docker build -t fmasa/lebeda:7.3-ci lebeda/7.3-ci --no-cache
 
