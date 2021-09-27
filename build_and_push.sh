1271e919ba31e8cf153232b26bf516041e89e054
Jan Staněk <jan@jstanek.cz>
2021-09-27 14:02:16 +0200

    php 8.0

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+docker build -t fmasa/lebeda:8.0 lebeda/8.0 --no-cache
+docker build -t fmasa/lebeda:8.0-ci lebeda/8.0-ci --no-cache
+
 docker build -t fmasa/lebeda:7.4 lebeda/7.4 --no-cache
 docker build -t fmasa/lebeda:7.4-ci lebeda/7.4-ci --no-cache
 
